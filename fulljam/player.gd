0b1206682aeb70e936fcdc257d1e01bcc2f8420e
spicode <126559883+spicode@users.noreply.github.com>
2025-08-24 20:04:11 +0300

    timer to player

--- fulljam/player.gd
+++ fulljam/player.gd
@@ -2,6 +2,8 @@ extends Node2D
 var card_col:Array[String]=["heart","spade","dimond","clubs"]
 var card_val:Array[String]=["1","2","3","4","5","6","7","8","9","J","Q","K","Ace"]
 func _ready() -> void:
-	var card:Array[String]=[card_col[randi_range(0,4)],card_val[randi_range(0,14)]]	
+	var decay :int = 30
+	var card:Array=[card_col[randi_range(0,4)],card_val[randi_range(0,14)],decay]
+	
 	print(card)
 var cards = {}
