1c7d81105c78223e66616b73baea23583ba7312c
spicode <126559883+spicode@users.noreply.github.com>
2025-08-24 20:23:05 +0300

    pick random

--- fulljam/player.gd
+++ fulljam/player.gd
@@ -3,7 +3,7 @@ var 	card_col:Array[String]=["heart","spade","dimond","clubs"]
 var 	card_val:Array[String]=["1","2","3","4","5","6","7","8","9","J","Q","K","Ace"]
 func _ready() -> void:
 	var 	decay :int = 30
-	for i in range(5):
-		var card:Array=[card_col[randi_range(0,4)],card_val[randi_range(0,14)],decay]	
-		print(card)
+	
+	var 	card:Array=[card_col.pick_random(),card_val.pick_random(),decay]	
+	print(card)
 var 	cards = {}
