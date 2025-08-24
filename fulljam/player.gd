924e5172d9501397ed20016cf21b17e632b99321
Javascript-hater <keidarims1@gmail.com>
2025-08-24 20:30:45 +0300

    card system (enemy and player)
    cool

--- fulljam/player.gd
+++ fulljam/player.gd
@@ -1,9 +1,22 @@
 extends Node2D
-var card_col:Array[String]=["heart","spade","dimond","clubs"]
-var card_val:Array[String]=["1","2","3","4","5","6","7","8","9","J","Q","K","Ace"]
-func _ready() -> void:
-	var decay :int = 30
-	var card:Array=[card_col[randi_range(0,4)],card_val[randi_range(0,14)],decay]
-	
-	print(card)
-var cards = {}
+
+var card_col = ["heart","spade","diamond","clubs"]
+var card_val = ["1","2","3","4","5","6","7","8","9","J","Q","K","Ace"]
+
+func _ready():
+	var player_cards = []
+	var enemy_cards = []
+
+	print("player cards:")
+	for i in range(5):
+		var decay = 30
+		var card = [card_col.pick_random(), card_val.pick_random(), decay]
+		player_cards.append(card)
+		print(card)
+
+	print("enemy cards:")
+	for i in range(5):
+		var decay = 30
+		var card = [card_col.pick_random(), card_val.pick_random(), decay]
+		enemy_cards.append(card)
+		print(card)
