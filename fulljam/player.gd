c5c7223af614a5c4bd78bb6299961826a1a0a9f9
spicode <126559883+spicode@users.noreply.github.com>
2025-08-24 20:14:40 +0300

    run multi time random

--- fulljam/player.gd
+++ fulljam/player.gd
@@ -1,9 +1,9 @@
 extends Node2D
-var card_col:Array[String]=["heart","spade","dimond","clubs"]
-var card_val:Array[String]=["1","2","3","4","5","6","7","8","9","J","Q","K","Ace"]
+var 	card_col:Array[String]=["heart","spade","dimond","clubs"]
+var 	card_val:Array[String]=["1","2","3","4","5","6","7","8","9","J","Q","K","Ace"]
 func _ready() -> void:
-	var decay :int = 30
-	var card:Array=[card_col[randi_range(0,4)],card_val[randi_range(0,14)],decay]
-	
-	print(card)
-var cards = {}
+	var 	decay :int = 30
+	for i in range(5):
+		var card:Array=[card_col[randi_range(0,4)],card_val[randi_range(0,14)],decay]	
+		print(card)
+var 	cards = {}
