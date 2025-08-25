2bfcbeb614c8a430e8c51df556e296547c6409e2
spicode <126559883+spicode@users.noreply.github.com>
2025-08-25 13:30:04 +0300

    delete

--- fulljam/player.gd
+++ fulljam/player.gd
@@ -3,6 +3,7 @@ var PokerHand = preload("res://poker_hands(Im_in_pain).gd").new()
 var card_col = ["heart","spade","diamond","clubs"]
 var card_val = ["1","2","3","4","5","6","7","8","9","J","Q","K","Ace"]
 var dead_card=[]
+var winner : String= ""
 var val_rank={
 	"2": 2, "3": 3, "4": 4, "5": 5,
 	"6": 6, "7": 7, "8": 8, "9": 9,
@@ -23,20 +24,19 @@ func _ready():
 		player_cards.append(card)
 		print(card)
 		
-		
+	
 	print("enemy cards:")
 	for i in range(5):
 		var decay = 30
 		var card = [card_col.pick_random(), card_val.pick_random(), randi_range(30,50)]
 		enemy_cards.append(card)
 		print(card)
-		
-	print(checkWinner())
+	winner = checkWinner()
+	
 func checkWinner() -> String:
 	var result = PokerHand.evaluate_hand(player_cards)
 	var bad_result = PokerHand.evaluate_hand(enemy_cards)
-	print("You(player) got a ", result)
-	print("ze bad(enemy) got a ",bad_result)
+	
 	if result.points > bad_result.points:
 		return "player"
 	elif result.points < bad_result.points:
@@ -50,12 +50,12 @@ func _process(delta: float) -> void:
 		#print("Card timer:", card[2]) 
 		if card[2] <= 0:
 			to_remove.append(card)
-			print("Card expired:", card)
+			
 	
 	for card in to_remove:
 		dead_card.append(card)
 		player_cards[player_cards.find(card)]=[card_col.pick_random(), card_val.pick_random(), randi_range(30,50)]
-		print(checkWinner())
+		winner = checkWinner()
 		
 		
 		
